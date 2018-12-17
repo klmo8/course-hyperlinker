72429fd4733eb1ec85cb7fcd54c8ebf7b37414aa
Kyle <kyelmo8@gmail.com>
2018-12-17 15:56:32 -0800

    handled error resulting from script injecting on chrome://extensions page

--- background.js
+++ background.js
@@ -1,10 +1,17 @@
 // Listening for sites (ie: Reddit) that use pushState / replaceState.
 // These sites replace the page's history state instead of actual refresh/reload, whenever possible.
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  chrome.tabs.executeScript(null,{file:"contentscript.js"});
+  // chrome.tabs.executeScript(null,{file:"contentscript.js"});
   // Adding the setInterval to run script every 5 seconds.
   // This is a hacky way of dealing with the infinite scroll feature of certain websites (Reddit, Twitter, etc).
   setInterval(function(){
-    chrome.tabs.executeScript(null,{file:"contentscript.js"});
-  }, 5000);
+    chrome.tabs.executeScript(tabID,{file:"contentscript.js"}, _=> {
+      // Handling error that results from trying to inject the script on chrome://extensions page
+      // Adapted from: https://stackoverflow.com/questions/26517988/unchecked-runtime-lasterror-while-running-tabs-executescript/45603880#45603880
+      let e = chrome.runtime.lastError;
+      if(e !== undefined){
+        console.log(tabId, _, e);
+      }
+    }, 5000);
+  });
 });

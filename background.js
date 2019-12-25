7ade9861d0531a9533c23033ac45cbf2a77ae145
Kyle <kyelmo8@gmail.com>
2019-12-25 07:37:14 -0800

    removed outdated comments

--- background.js
+++ background.js
@@ -1,14 +1,12 @@
 // Listening for sites (ie: Reddit) that use pushState / replaceState.
 // These sites replace the page's history state instead of actual refresh/reload, whenever possible.
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  // Adding the setInterval to run script every 5 seconds.
-  // This is a hacky way of dealing with the infinite scroll feature of certain websites (Reddit, Twitter, etc).
-    chrome.tabs.executeScript({file:"contentscript.js"}, _=> {
-      // Handling error that results from trying to inject the script on chrome://extensions page
-      // Adapted from: https://stackoverflow.com/questions/26517988/unchecked-runtime-lasterror-while-running-tabs-executescript/45603880#45603880
-      let e = chrome.runtime.lastError;
-      if(e !== undefined){
-        console.log(_, e);
-      }
-    })
+  chrome.tabs.executeScript({ file: "contentscript.js" }, _ => {
+    // Handling error that results from trying to inject the script on chrome://extensions page
+    // Adapted from: https://stackoverflow.com/questions/26517988/unchecked-runtime-lasterror-while-running-tabs-executescript/45603880#45603880
+    let e = chrome.runtime.lastError;
+    if (e !== undefined) {
+      console.log(_, e);
+    }
   });
+});
